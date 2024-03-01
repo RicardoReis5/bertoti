92e9ffb0fafa4d05ad8ae45d8e119cbca460660d
RicardoReis5 <127327058+RicardoReis5@users.noreply.github.com>
2024-03-01 12:04:22 -0300

    feat-controllers-and-class
    added Car class
    added GET, POST, PUT and DELETE controllers

--- lab3/meu primeiro REST/src/main/java/com/example/demo/DemoApplication.java
+++ lab3/meu primeiro REST/src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,98 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+}
+@RestController
+@RequestMapping("/cars")
+class RestApiDemoController {
+	private List<Car> cars = new ArrayList<>();
+
+	public RestApiDemoController() {
+		cars.addAll(List.of(
+				new Car("Volkswagem Fusca"),
+				new Car("Fiat Argo"),
+				new Car("Ford Ecosport"),
+				new Car("BMW 320i"),
+				new Car("Toyota Corolla")
+		));
+}
+
+	@GetMapping
+	Iterable<Car> getCars() {
+		return cars;
+	}
+
+	@GetMapping("/{id}")
+	Optional<Car> getCarById(@PathVariable String id) {
+		for (Car c: cars) {
+			if  (c.getId().equals(id)){
+				return Optional.of(c);
+			}
+		}
+		return Optional.empty();
+	}
+
+	@PostMapping
+	Car postCar(@RequestBody Car car) {
+		cars.add(car);
+		return car;
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Car> putCar(@PathVariable String id,
+							   @RequestBody Car car) {
+
+		int carIndex = -1;
+
+		for (Car c: cars) {
+			if (c.getId().equals(id)) {
+				carIndex = cars.indexOf(c);
+				cars.set(carIndex, car);
+			}
+		}
+
+		return (carIndex == -1) ?
+				new ResponseEntity<>(postCar(car), HttpStatus.CREATED) :
+				new ResponseEntity<>(car, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteCar(@PathVariable String id) {
+		cars.removeIf(c -> c.getId().equals(id));
+	}
+}
+
+class Car {
+	private final String id;
+	private String name;
+	public Car(String id, String name){
+		this.id = id;
+		this.name = name;
+	}
+	public Car(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 }
